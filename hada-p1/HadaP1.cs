c467ea916d9b9f90679263d3d346ceb938612810
sn26 <sn26@alu.ua.es>
2019-02-17 19:54:54 +0100

    Modified HadaP1 class

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -10,7 +10,10 @@ namespace hada_p1
     {
         public static double Seconds2Minutes(double s)
         {
-            
+            if (s == 0)
+            {
+                return 0;
+            }
             return s / 60;
 
         }
