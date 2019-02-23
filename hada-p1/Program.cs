7d5b7f634f8ddae50b9ebb26e56c43f136a73906
sn26 <sn26@alu.ua.es>
2019-02-23 20:10:20 +0100

    Added call to functions

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Clase program, dondehemos realizado la llamada a las funciones implementadas en hadaP1.cs
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// método que muestra por pantalla la llamadad a las diferentes funciones de hadap1.cs 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             bool fin = false;
@@ -28,6 +35,11 @@ namespace hada_p1
                     Console.WriteLine("Cantidad: ");
                     pregunta = Console.ReadLine();
                     Console.WriteLine("Resultado:" + HadaP1.Minutes2Seconds(double.Parse(pregunta)));
+                }else if ( pregunta== "H" || pregunta == "h")
+                {
+                    Console.WriteLine("Cantidad: ");
+                    pregunta = Console.ReadLine();
+                    Console.WriteLine("Resultado:" + HadaP1.Hours2Minutes(double.Parse(pregunta)));
                 }
                 Console.WriteLine("Seguir (Y/N) : ");
                 pregunta = Console.ReadLine();
