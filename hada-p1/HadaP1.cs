d4e36cc9e4ca53d2a08724acee9d875daec4ff37
sn26 <sn26@alu.ua.es>
2019-02-23 20:00:21 +0100

    Added comments HadaP1 class

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Clase Hada-P1, donde tenemos los métodos que se utilizan para cambiar de unidad
+    /// </summary>
     class HadaP1
     {
+        /// <summary>
+        /// Método que me sirve para cambiat de segundos a minutos 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s)
         {
             if (s == 0)
@@ -17,7 +25,11 @@ namespace hada_p1
             return s / 60;
 
         }
-
+        /// <summary>
+        /// Método que me sirve para cambiar de minutos a segundos 
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public static double Minutes2Seconds(double m)
         {
             return 60 * m;
